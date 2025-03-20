52775369c744ab3552a87d4d5e528c5dc55e6a40
Hugo Grimmett <hugo.grimmett@gmail.com>
2025-03-20 12:03:00 +0100

    page now resizes more gracefully, but pdf boundary broken

--- renderer.js
+++ renderer.js
@@ -625,9 +625,12 @@ class BoxRenderer {
     
     getMousePoint(e) {
         const rect = this.canvas.getBoundingClientRect();
+        // Account for CSS scaling
+        const scaleX = this.canvas.width / rect.width;
+        const scaleY = this.canvas.height / rect.height;
         return this.inverseTransform({
-            x: e.clientX - rect.left,
-            y: e.clientY - rect.top
+            x: (e.clientX - rect.left) * scaleX,
+            y: (e.clientY - rect.top) * scaleY
         });
     }
     
